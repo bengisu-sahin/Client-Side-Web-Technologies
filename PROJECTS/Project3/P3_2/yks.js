b2a4ea6a977fc202c0b9bd90ffa6edf274775fc3
bengisu-sahin <bengisusahin99@gmail.com>
2023-04-11 21:11:00 +0300

    Some js code is added.

--- PROJECTS/Project3/P3_2/yks.js
+++ PROJECTS/Project3/P3_2/yks.js
@@ -3,30 +3,38 @@ let dersler = [
         dersAdi: "tytTurkce",
         toplamSoruSayisi: 40,
         dogru: 0,
-        yanlis: 0
+        yanlis: 0,
+        net: 0,
     },
+
     {
         dersAdi: "tytSosyal",
         toplamSoruSayisi: 20,
         dogru: 0,
-        yanlis: 0
+        yanlis: 0,
+        net:0,
     }
 ];
-
-var inputs = document.querySelectorAll("input");
-for (var i = 0; i < inputs.length; i++) {
-    let name = inputs[i].className;
-
-    if (dersler.find(obj => obj.dersAdi === name)) {
-        console.log(name);
-        let ders=dersler.find(obj => obj.dersAdi === name);
-        let id=
-        // console.log("classname");
-        // console.log(name);
-        // let bob_object = dersler.find(obj => obj.dersAdi === name);
-        // console.log("nesne");
-        // console.log(bob_object.toplamSoruSayisi); // { name: "Bob", age: 35, city: "Chicago" }
-    }
-
-
+function showNet(ders) {
+    let net=ders.dogru-(0.25*ders.yanlis);
+    return net;
 }
+/**
+ *change olayı, kullanıcının input alanından odaklanması kaybolduğunda tetiklenir. 
+ Eğer herhangi bir girdi yapıldığında hemen tetiklenmesini istediğimiz için input event ini kullandım
+*/
+inputs.forEach(input => {
+    input.addEventListener('input', function () {
+        let className = input.className;
+        let ders = dersler.find(obj => obj.dersAdi === className);
+        console.log(ders.dersAdi);
+        if (ders) {
+             ders.dogru=document.getElementById(ders.dersAdi + 'Dogru').value;
+             console.log(ders.dogru);
+             ders.yanlis=document.getElementById(ders.dersAdi + 'Yanlis').value; 
+             console.log(ders.yanlis);
+            document.getElementById(ders.dersAdi + 'Net').value = showNet(ders);
+            console.log(document.getElementById(ders.dersAdi + 'Net').value);
+        }
+    });
+});
