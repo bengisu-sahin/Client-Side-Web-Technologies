10bd93610dba9277e2f87c91c3cf95f440f0a72c
bengisu-sahin <bengisusahin99@gmail.com>
2023-04-13 16:37:43 +0300

    Total Question Check is done.

--- PROJECTS/Project3/P3_2/yks.js
+++ PROJECTS/Project3/P3_2/yks.js
@@ -5,32 +5,37 @@ let dersler = [
     { dersAdi: "aytDin", toplamSoruSayisi: 6, dogru: 0, yanlis: 0, net: 0 }, { dersAdi: "aytMat", toplamSoruSayisi: 40, dogru: 0, yanlis: 0, net: 0 }, { dersAdi: "aytFizik", toplamSoruSayisi: 14, dogru: 0, yanlis: 0, net: 0 }, { dersAdi: "aytKimya", toplamSoruSayisi: 13, dogru: 0, yanlis: 0, net: 0 }, { dersAdi: "aytBiyoloji", toplamSoruSayisi: 13, dogru: 0, yanlis: 0, net: 0 },
 
 ];
+
 function showNet(ders) {
     let net = ders.dogru - (0.25 * ders.yanlis);
     return net;
 }
+
 function makeAlert(dogru, yanlis, toplamSoru) {
-    if( dogru+yanlis > toplamSoru){
-        alert("Toplamda `{toplamSoru}` soru girebilirsin.");
+    if (dogru + yanlis > toplamSoru) {
+        window.alert("Toplamda " + toplamSoru.toString() + " soru girebilirsin.");
+        return true;
     }
-    return;
+    return false;
 }
-/**
- *change olayı, kullanıcının input alanından odaklanması kaybolduğunda tetiklenir. 
- Eğer herhangi bir girdi yapıldığında hemen tetiklenmesini istediğimiz için input event ini kullandım
-*/
+
 const inputs = document.querySelectorAll('input');
 inputs.forEach(input => {
-    input.addEventListener('input', function () {
+    input.addEventListener('change', function () {
         let className = input.className;
         let ders = dersler.find(obj => obj.dersAdi === className);
         console.log(ders.dersAdi);
         if (ders) {
-            ders.dogru = document.getElementById(ders.dersAdi + 'Dogru').value;
-            ders.yanlis = document.getElementById(ders.dersAdi + 'Yanlis').value;
-            makeAlert(ders.dogru, ders.yanlis, ders.toplamSoruSayisi);
-            document.getElementById(ders.dersAdi + 'Net').value = showNet(ders);
-            
+            ders.dogru = Number(document.getElementById(ders.dersAdi + 'Dogru').value);
+            ders.yanlis = Number(document.getElementById(ders.dersAdi + 'Yanlis').value);
+            if (makeAlert(ders.dogru, ders.yanlis, ders.toplamSoruSayisi) == true) {
+                document.getElementById(ders.dersAdi + 'Dogru').value = "-";
+                document.getElementById(ders.dersAdi + 'Yanlis').value = "-";
+                document.getElementById(ders.dersAdi + 'Net').value = "-";
+            }
+            else {
+                document.getElementById(ders.dersAdi + 'Net').value = showNet(ders);
+            }
         }
     });
 });
