3dd0bb6771931c2c1956e6bd7ca2bd97485667af
Bengisu <71591780+bengisu-sahin@users.noreply.github.com>
2023-04-24 21:59:05 +0300

    Delete React directory

--- React/01-FirstReactExamples/s5.js
+++
@@ -1,7 +0,0 @@
-// Define the Car component
-function Car() { 
-    return <h2>Hi, I am a Car!</h2>; 
-}
-
-// Render the Car component using ReactDOM.render
-ReactDOM.render(<Car />, document.getElementById('root'));
--- React/01-FirstReactExamples/s6.js
+++
@@ -1,8 +0,0 @@
-class Car extends React.Component{
-    render(){
-        return <h2>Hi, I am a Car!</h2>
-    }
-    
-}
-//To render this component inside ReactDOM.rende using JSX, vall it as follows:
-ReactDOM.render(<Car />, document.getElementById('root'));
