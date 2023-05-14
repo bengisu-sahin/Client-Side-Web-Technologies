555c01a9d793cb7a7bb3d562281b733b22654b51
bengisu-sahin <bengisusahin99@gmail.com>
2023-05-14 21:55:43 +0300

    Start to implement calculation function.

--- PROJECTS/Project4/P4_2/calc.js
+++ PROJECTS/Project4/P4_2/calc.js
@@ -16,3 +16,60 @@ modeBtn.addEventListener("click", function () {
     }
 });
 
+
+
+function checkElement(buttonValue){
+    if(buttonValue=='C'){
+        return 0;
+    }
+    else if(buttonValue=='/' || buttonValue=='*' || buttonValue=='+' || buttonValue=='-' ){
+        return 1;
+    }
+    else if(buttonValue == '='){
+        return 2;
+    }
+    else{ //number
+        return 3;
+    }
+}
+
+// var pressedBtns=document.querySelectorAll(".arg");
+// console.log(pressedBtns);
+// var param1 = 0;
+// var param2 = 0;
+// var operator = '';
+// var []=
+const inputs = {
+    param1: 0,
+    operator: "",
+    param2: 0
+  };
+const screen=document.getElementById("display");
+console.log(screen);
+const buttonsContainer = document.querySelector('.container');
+buttonsContainer.addEventListener('click', function(event) {
+
+/*Bu kod, bir olayın hedefinin buton elementi olup olmadığını kontrol eder.Olay dinleyicisi tarafından alınan event nesnesinin target özelliği, olayın hedefi olan HTML öğesini temsil eder. tagName özelliği ise bu öğenin etiket adını büyük harflerle döndürür.*/
+
+  if (event.target.tagName === 'BUTTON') {
+
+    const buttonValue = event.target.innerText;
+    console.log(buttonValue);
+    var btnType=checkElement(buttonValue);
+
+    if(btnType == 0){ //reset screen
+       screen.innerText='';
+    }
+    else if(btnType == 1 ){ //operator
+        inputs.operator=buttonValue;
+        
+    }
+    else if(btnType == 2 ){ //result
+
+        screen.innerText='111';
+     }
+    else{ //number
+
+    }
+  }
+});
